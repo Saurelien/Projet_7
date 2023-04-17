96f199dbdf1b3c7113ef279c3032b40359ad2179
Saurelien <aurelienschmitt.info@gmail.com>
2023-04-17 13:46:34 +0200

    Switching from main.py to brute_force.py all the combinations process + adding the time and the resources needed to execute the script

--- Brute_Force.py
+++ Brute_Force.py
@@ -1,68 +1,64 @@
+import itertools
 from itertools import combinations
+import time
+import psutil
 
-item_list = ["A", "B", "C"]
-print(item_list)
-
-def get_combinations(item_list):
-    data = list()
-    for i in combinations(data, 20):
-        data.append(item_list[i])
-    return data
-
-
-print(get_combinations("20"))
-# [["A"], ["B"], ["C"], ["A", "B"], ["A", "C"], ["B", "C"], ["A", "B", "C"]]
-
-COMBINATIONS = {
-    "action-1":
-        {"Price": 20, "Interest": 5},
-    "action-2":
-        {"Price": 30, "Interest": 10},
-    "action-3":
-        {"Price": 50, "Interest": 15},
-    "action-4":
-        {"Price": 70, "Interest": 20},
-    "action-5":
-        {"Price": 60, "Interest": 17},
-    "action-6":
-        {"Price": 80, "Interest": 25},
-    "action-7":
-        {"Price": 22, "Interest": 7},
-    "action-8":
-        {"Price": 26, "Interest": 11},
-    "action-9":
-        {"Price": 48, "Interest": 13},
-    "action-10":
-        {"Price": 34, "Interest": 27},
-    "action-11":
-        {"Price": 42, "Interest": 17},
-    "action-12":
-        {"Price": 110, "Interest": 9},
-    "action-13":
-        {"Price": 38, "Interest": 23},
-    "action-14":
-        {"Price": 14, "Interest": 1},
-    "action-15":
-        {"Price": 18, "Interest": 3},
-    "action-16":
-        {"Price": float(0.8), "Interest": 8},
-    "action-17":
-        {"Price": float(0.4), "Interest": 12},
-    "action-18":
-        {"Price": 10, "Interest": 14},
-    "action-19":
-        {"Price": 24, "Interest": 21},
-    "action-20":
-        {"Price": 114, "Interest": 18},
-                }
-WALLET = 500
+NEW_COMBINATIONS = [
+    ("#1", 20, 5),
+    ("#2", 30, 10),
+    ("#3", 50, 15),
+    ("#4", 70, 20),
+    ("#5", 60, 17),
+    ("#6", 80, 25),
+    ("#7", 22, 7),
+    ("#8", 26, 11),
+    ("#9", 48, 13),
+    ("#10", 34, 27),
+    ("#11", 42, 17),
+    ("#12", 110, 9),
+    ("#13", 38, 23),
+    ("#14", 14, 1),
+    ("#15", 18, 3),
+    ("#16", float(0.8), 8),
+    ("#17", float(0.4), 12),
+    ("#18", 10, 14),
+    ("#19", 24, 21),
+    ("#20", 114, 18)
+                   ]
 
 
-#Boucler sur le dictionnaire pour en sortie les 2 valeurs correspondant à chaques actions
+def all_combinations(actions_list):
+    combinations = []
+    for all_combinations in range(1, len(actions_list) + 1):
+        for combination in itertools.combinations(actions_list, all_combinations):
+            combinations.append(combination)
+    return combinations
 
 
-#def all_combinations(action, price, interest)0:
-#
-#    combi = COMBINATIONS
-#    for i in combinations(combi, 500):
-#        print(i)
+def get_gain(actions_list, budget_wallet=500):
+    best_yield = 0
+    best_combination = []
+    #Appel de la fonction " all_combination " dans la boucle des combinaisons possible de la liste en dur
+    for combination in all_combinations(actions_list):
+        for action in combination:
+            #calcule du rendement " action[1] correspond a l'élément 2 = prix de l'action
+            capital = int(budget_wallet / action[1])
+            rendement = capital * action[2] / 100
+        if rendement > best_yield:
+            best_yield = rendement
+            best_combination = combination
+        # calcule temps de processus:
+        process_time = time.process_time()
+        progress = psutil.Process()
+        cpu_percent = progress.cpu_percent()
+        memory_usage = progress.memory_percent()
+    return best_combination, best_yield, cpu_percent, memory_usage, process_time
+
+
+best_combination, best_yield, cpu_percent, memory_usage, process_time = get_gain(NEW_COMBINATIONS, 500)
+print(f"Temps d'execution: {process_time}")
+print(f"Meilleur combinaison: {best_combination}")
+print(f"Meilleur rendement: {best_yield}")
+print(f"Utilisation du processeur: {cpu_percent} %")
+print(f"Utilisation de la RAM: {memory_usage} %")
+
