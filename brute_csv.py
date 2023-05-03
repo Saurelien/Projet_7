125b5c87e3e5eb10c2049b17b71669c568f9506f
Saurelien <aurelienschmitt.info@gmail.com>
2023-05-03 19:03:36 +0200

    Uses of a gluttonous algorithm to optimize the csv reading and filtering the best action based on the yield of each stock action

--- brute_csv.py
+++ brute_csv.py
@@ -2,112 +2,65 @@ import time
 import psutil
 import csv
 
-
-# calcule temps de processus:
-PROCESS_TIME = time.process_time()
-# Utilisation en ressource
-PROGRESS = psutil.Process()
 PATH = "dataset1_Python+P7.csv"
 PATH_2 = "dataset2_Python+P7.csv"
 
 
-def get_actions_from_csv(file_path_1):
+def get_actions_from_csv(file_path_1, budget=500):
     """TEST, Cette fonction prend en entrée deux chemins de fichier type csv et renvoie une liste
     contenant les données du fichier."""
 
-    # Récupération et convertion de " dataset1_Python+P7.csv "
-    print("Lecture de dataset1_Python+P7.csv...")
+    # Décompte du temps
+    start_time = time.monotonic()
+    # Objet pour traquer l'utilisation du cpu
+    process = psutil.Process()
+    # Objet pour traquer l'utilisation de la mémoire
+    mem_before = process.memory_info().rss
+
+    # Récupération et convertion du fichier csv
     with open(file_path_1, 'r') as file:
         reader = csv.reader(file)
-        next(reader)  # ignorer l'entête
+        # Ignorer l'en-tête
+        next(reader)
+        actions = [(name, float(price), float(profit), (int(budget) / float(price)) * float(profit) / 100)
+                   for name, price, profit in reader
+                   if float(price) > 0 and float(profit) > 0]
 
-        # Cette liste contient pour chaque action, les informations suivantes : nom, prix, bénéfice et rendement
-        # (calculé en multipliant le prix par le bénéfice, le tout divisé par 100).
-        # Ainsi qu'une condition eliminant les données à zero ou inférieur du prix et profit de la liste
-        action_list1 = [
-            (name, float(price), float(profit), float(price) * float(profit) / 100)
-            for name, price, profit in reader
-            if float(price) > 0 and float(profit) > 0
-        ]
+    # Trier les actions en fonction de leur rendement, du plus élevé au plus faible
+    actions = sorted(actions, key=lambda action: action[3], reverse=True)
 
-    # Utilisation de la fonction "max" pour déterminer l'action la plus rentable.
-    # Cette action est déterminée en utilisant le rendement comme clé de tri.
-    best_action_yield = max(action_list1, key=lambda best_yield: best_yield[3])
-    print(best_action_yield)
+    # Sélectionner les actions ayant le rendement le plus élevé, jusqu'à atteindre le budget maximal de 500$
+    best_actions = []
+    for action in actions:
+        if action[1] <= budget:
+            # tant que le prix de l'action est inferieur au 500$, j'ajoute:
+            # Le nom, Le prix, Le profit, Le rendement dans la liste best_actions
+            best_actions.append({
+                "name": action[0],
+                "price": action[1],
+                "profit": action[2],
+                "yield": action[3],
+            })
+            budget -= action[1]
+        if budget <= 0:
+            break
 
-    return action_list1
+    # Utilisation de la ram pendant l'execution de la fonction
+    mem_after = process.memory_info().rss
+    # Utilisation du cpu pendant le processus en %
+    cpu_usage = process.cpu_percent()
+    # Calcule du temps écoulé
+    elapsed_time = time.monotonic() - start_time
 
-def get_price_for_combination(combination):
-    total_price = 0
-    for action in combination:
-        total_price += action[1]
-    return total_price
+    # Print elapsed time, CPU usage, and memory usage
+    print(f"Temps écoulé: {elapsed_time:.3f} seconds")
+    print(f"Utilisation du cpu: {cpu_usage}%")
+    print(f"utilisation de la mémoire: {(mem_after - mem_before) / 1024 / 1024:.2f} MB")
 
-
-def get_gain_for_combination(combination1):
-    total = 0
-    for action in combination1:
-        total += get_gain(action)
-    return total
-
-
-def get_best(actions_list, wallet=500):
-    # Utilisation actuelle du CPU:
-    start_cpu_percent = psutil.cpu_percent()
-
-    # Utilisation actuelle de la mémoire:
-    process = psutil.Process()
-    start_mem = process.memory_info().rss / 1024 / 1024
-
-    # Début de la mesure du temps d'exécution
-    start_time = time.time()
-
-    combinations = get_actions_from_csv(PATH)
-    best_combination = None
-    best_yield = 0
-    for combination in combinations:
-        # check action price
-        total_price = get_price_for_combination(combination)
-        if float(total_price) > float(wallet):
-            continue
-
-        # check gain
-        rendement = get_gain_for_combination(combination)
-        if rendement > best_yield:
-            best_combination = combination
-            best_yield = rendement
-
-            # Fin de la mesure du temps d'execution:
-            end_time = time.time()
-
-            # Consommation de la mémoire à la fin
-            end_mem = process.memory_info().rss / 1024 / 1024
-
-            # % de cpu consommé:
-            end_cpu_percent = psutil.cpu_percent()
-
-            # Calculer le temps de lecture
-            elapsed_time = end_time - start_time
-
-            # Calculer la consommation de mémoire
-            mem_usage = end_mem - start_mem
-
-            # Afficher les informations sur les performances :
-            print(f"Temps de lecture : {elapsed_time:.2f} secondes")
-            print(f"Consommation de mémoire : {mem_usage:.2f} MB")
-            print(f"Utilisation CPU : {end_cpu_percent - start_cpu_percent:.2f} %")
-            if memory_usage >= 99 and process_time > 300:
-                break
-    return best_combination, best_yield, process_time, cpu_percent, memory_usage
+    return best_actions
 
 
 action_list1 = get_actions_from_csv(PATH)
-best_combination, best_yield, process_time, cpu_percent, memory_usage = get_best(action_list1)
-
-
-print(f"Meilleur combinaison: {best_combination}\n"
-      f"Meilleur rendement: {best_yield} € \n"
-      f"Meilleur prix: {get_price_for_combination(best_combination)} \n"
-      f"Temps d'execution: {process_time} secondes \n"
-      f"cpu usage: {cpu_percent} % \n"
-      f"Usage de la Ram: {memory_usage} %")
+print(action_list1)
+action_list2 = get_actions_from_csv(PATH_2)
+print(action_list2)
