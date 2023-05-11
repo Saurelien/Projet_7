63fd3fd2b1546791b3fac4176bdcc427c398e827
Saurelien <aurelienschmitt.info@gmail.com>
2023-05-11 23:04:59 +0200

    refactoring the csv function returning a filtered list from a path file and created a function to use the list and budget to get the best actions from the list

--- Brute_Force.py
+++ Brute_Force.py
@@ -1,6 +1,4 @@
 import itertools
-import csv
-from itertools import combinations
 import time
 import psutil
 
--- optimized.py
+++ optimized.py
@@ -1,14 +1,42 @@
 import time
 import psutil
 import csv
+import matplotlib.pyplot as plt
 
 PATH = "dataset1_Python+P7.csv"
 PATH_2 = "dataset2_Python+P7.csv"
 
 
-def get_actions_from_csv(file_path_1, budget=500):
+def get_actions_from_csv(file_path_1):
     """TEST, Cette fonction prend en entrée un chemins de fichier type csv et renvoie une liste
     contenant les données du fichier."""
+
+    # Récupération et convertion du fichier csv
+    with open(file_path_1, 'r') as file:
+        reader = csv.reader(file)
+        # Ignorer l'en-tête
+        next(reader)
+
+        # Structuration de la liste
+        action_list = []
+        for name, price, profit in reader:
+            price = float(price)
+            profit = float(profit)
+            if price > 0 and profit > 0:
+                action_list.append((name, price, profit, price * profit / 100))
+
+    # Trier les actions en fonction de leur rendement, du plus élevé au plus faible
+    action_list = sorted(action_list, key=lambda action: action[3], reverse=True)
+    return action_list
+
+
+def calculate_total_return(actions):
+    total_cost = sum(action["price"] for action in actions)
+    total_profit = sum(action["profit"] for action in actions)
+    return total_cost * total_profit / 100
+
+
+def get_action_best_yield(action_list, budget=500):
 
     # Décompte du temps
     start_time = time.monotonic()
@@ -17,30 +45,10 @@ def get_actions_from_csv(file_path_1, budget=500):
     # Objet pour traquer l'utilisation de la mémoire
     mem_before = process.memory_info().rss
 
-    # Récupération et convertion du fichier csv
-    with open(file_path_1, 'r') as file:
-        reader = csv.reader(file)
-        # Ignorer l'en-tête
-        next(reader)
-
-        # Structuration de la liste en intention ou liste de compréhension
-        action_list = [
-            (
-                name,
-                float(price),
-                float(profit),
-                float(price) * float(profit) / 100
-            )
-            for name, price, profit in reader
-            if float(price) > 0 and float(profit) > 0
-            and float(price) * float(profit) / 100 > 0
-        ]
-
-    # Trier les actions en fonction de leur rendement, du plus élevé au plus faible
-    action_list = sorted(action_list, key=lambda action: (action[3], action[1]), reverse=True)
-
     # Sélectionner les actions ayant le rendement le plus élevé, jusqu'à atteindre le budget maximal de 500$
     best_actions = []
+    best_action_pair = None
+    best_yield = 0
     for action in action_list:
         if action[1] <= budget:
             # Si le prix de l'action est inferieur a 500$, j'ajoutes:
@@ -49,21 +57,19 @@ def get_actions_from_csv(file_path_1, budget=500):
                 "name": action[0],
                 "price": action[1],
                 "profit": action[2],
-                "rendement réelle": action[3]
+                "yield": action[3]
             })
             budget -= action[1]
-        if budget <= 0:
-            break
-
-    # Afficher les résultats ligne par ligne pour une meilleurs visibilité
-    rows_array = []
-    for row in best_actions:
-        rows = f"Sienna bought: \n {row['name']} \n total cost: {row['price']}\n" \
-               f" Total return: {row['rendement réelle']}"
-        print(rows)
-        rows_array.append(rows)
-    #max_yield = max(rows_array, key=lambda best_yield: best_yield[3])
-    #print(max_yield)
+            print(f"\n name: {action[0]}, prix: {action[1]:2f}, profit: {action[2]:2f}"
+                  f"\n rendement réelle: {action[3]:2f}, budget restant: {budget:.2f}$ \n")
+            for i in range(len(best_actions)):
+                for j in range(i + 1, len(best_actions)):
+                    total_yield = calculate_total_return(best_actions)
+                    if total_yield > best_yield:
+                        best_action_pair = (best_actions[i], best_actions[j])
+                        best_yield = total_yield
+                    if budget <= 0:
+                        break
 
     # Utilisation de la ram pendant l'execution de la fonction
     mem_after = process.memory_info().rss
@@ -72,12 +78,15 @@ def get_actions_from_csv(file_path_1, budget=500):
     # Calcule du temps écoulé
     elapsed_time = time.monotonic() - start_time
 
-    print(f"Temps écoulé: {elapsed_time:.3f} seconds")
-    print(f"Utilisation du cpu: {cpu_usage}%")
-    print(f"utilisation de la mémoire: {(mem_after - mem_before) / 1024 / 1024:.2f} MB")
+    print(f"\n Budget final restant: {budget:.2f}$ \n Rendement réelle total sur deux ans: {best_yield}")
+    print(f"\n Temps écoulé: {elapsed_time:.3f} seconds")
+    print(f"\n Utilisation du cpu: {cpu_usage}%")
+    print(f"\n utilisation de la mémoire: {(mem_after - mem_before) / 1024 / 1024:.2f} MB")
 
-    return rows_array
+    return best_actions, best_action_pair
 
 
 action_list1 = get_actions_from_csv(PATH)
 action_list2 = get_actions_from_csv(PATH_2)
+get_action_best_yield(action_list1)
+get_action_best_yield(action_list2)
